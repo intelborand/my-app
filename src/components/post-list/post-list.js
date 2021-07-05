ae498dbae0ae630d7e9108b124dca6d31c2750b7
Andrzej <intelborand@gmail.com>
2021-07-05 19:05:24 +0200

    Complited

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import './post-list.css';
+import { ListGroup } from 'reactstrap';
 
 import PostListItem from '../post-list-item';
 
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
 
     const elements = posts.map((item) => {
+        const {id, ...itemProps} = item;
         return(
             <li key={item.id} className="list-group-item">
-            <PostListItem
-            label={item.label}
-            important={item.important} />
+                <PostListItem
+                    {...itemProps}
+                    onDelete={() => onDelete(id)}
+                    onToggleImportant={() => onToggleImportant(id)}
+                    onToggleLiked = {() => onToggleLiked(id)} />
             </li>
         )
     })
 
     return (
-        <ul className="app-list list-group">
+        <ListGroup className="app-list">
             {elements}
-        </ul>
+        </ListGroup>
     )
 }
 
